8b6896dced1734a45d46ddf29b5b9ed56dcafb4e
kunchon <1070800492@qq.com>
2019-04-10 11:07:23 +0800

    feat: 1.百度鹰眼轨迹       2.直播功能实现       3.签名功能实现

---
+++ app/src/main/java/cn/cdjzxy/monitoringassistant/trajectory/receiver/TrackReceiver.java
@@ -0,0 +1,47 @@
+package cn.cdjzxy.monitoringassistant.trajectory.receiver;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.PowerManager.WakeLock;
+import android.util.Log;
+
+import com.baidu.trace.model.StatusCodes;
+
+public class TrackReceiver extends BroadcastReceiver {
+    private static final String TAG = "TrackReceiver";
+    private WakeLock wakeLock = null;
+
+    public TrackReceiver(WakeLock wakeLock) {
+        super();
+        this.wakeLock = wakeLock;
+    }
+
+    @SuppressLint("Wakelock")
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (Intent.ACTION_SCREEN_OFF.equals(action)) {
+            if (null != wakeLock && !(wakeLock.isHeld())) {
+                wakeLock.acquire();
+            }
+        } else if (Intent.ACTION_SCREEN_ON.equals(action) || Intent.ACTION_USER_PRESENT.equals(action)) {
+            if (null != wakeLock && wakeLock.isHeld()) {
+                wakeLock.release();
+            }
+        } else if (StatusCodes.GPS_STATUS_ACTION.equals(action)) {
+            int statusCode = intent.getIntExtra("statusCode", 0);
+            String statusMessage = intent.getStringExtra("statusMessage");
+            System.out.println(String.format("GPS status, statusCode:%d, statusMessage:%s", statusCode,
+                    statusMessage));
+            Log.i(TAG, "onReceive: " + String.format("GPS status, statusCode:%d, statusMessage:%s", statusCode,
+                    statusMessage));
+            Log.e(TAG, "onReceive: " + String.format("GPS status, statusCode:%d, statusMessage:%s", statusCode,
+                    statusMessage));
+
+        }
+    }
+
+}
